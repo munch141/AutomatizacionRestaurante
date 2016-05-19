39cc8f4d6aee9c524aacd408fbd2c4b7f4563e30
ricardo <ricardo@ricardo-pc>
2016-05-19 17:32:14 -0430

    Detalles

--- AutomatizacionRestaurante/registro/views.py
+++ AutomatizacionRestaurante/registro/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.template.context_processors import request
-from django.http.response import HttpResponse
+
+app_name = 'registro'
 
 def registroCliente(request):
     return HttpResponse("Registro")
