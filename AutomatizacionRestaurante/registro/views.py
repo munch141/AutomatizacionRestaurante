418069939a480182d856733f491c01f8cc0ece1c
giuli <ricardo@ricardo-pc>
2016-05-19 17:51:34 -0430

    Vista:
    Creacion del formulario de registro de usuario.

--- AutomatizacionRestaurante/registro/views.py
+++ AutomatizacionRestaurante/registro/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.template.context_processors import request
 
 app_name = 'registro'
 
 def registroCliente(request):
-    return HttpResponse("Registro")
+    return render(request, 'registro/registroCliente.html')
