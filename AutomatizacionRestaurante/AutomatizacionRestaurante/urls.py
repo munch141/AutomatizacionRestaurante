be54400c36312414c3ff1650a95bbc4e2624f176
User1 <User1@User1-PC>
2016-05-19 16:15:23 -0400

    Base de Datos:
    Se agrega la tabla de cliente

--- AutomatizacionRestaurante/AutomatizacionRestaurante/urls.py
+++ AutomatizacionRestaurante/AutomatizacionRestaurante/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^registro/', include('registro.urls')),
+    url(r'^Registro/', include('Registro.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- AutomatizacionRestaurante/Registro/models.py
+++ AutomatizacionRestaurante/Registro/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.regex_helper import Choice
 
-# Create your models here.
+
+class Cliente(models.Model):
+    ci = models.PositiveIntegerField(primary_key=True)
+    nombre = models.CharField(max_length=60)
+    apellido = models.CharField(max_length=60)
+    fecha_nacimiento = models.DateTimeField('fecha de nacimiento')
+    email = models.EmailField
+    telefono = models.IntegerField()
+    SEXOS = (
+         ('M','Masculino'),
+         ('F','Femenino')    
+    )
+    sexo = models.CharField(max_length=1,choices=SEXOS)
+    clave = models.CharField(max_length=10)
+    
