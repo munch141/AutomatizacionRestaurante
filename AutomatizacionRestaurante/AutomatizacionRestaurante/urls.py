7bc79637d574c400d57611b946622ce0aafe641a
Ricardo Münch <munch_141@hotmail.com>
2016-06-03 20:35:19 -0400

    se cambio el url en la plantilla de registro exitoso a uno con nombre

--- AutomatizacionRestaurante/AutomatizacionRestaurante/urls.py
+++ AutomatizacionRestaurante/AutomatizacionRestaurante/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from home.views import LoginView, logout_view
 
 urlpatterns = [
-	url(r'^$', LoginView.as_view(), name='login'),
-	url(r'^logout/$', logout_view),
+    url(r'^$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', logout_view, name='logout'),
     url(r'^registro/', include('registro.urls')),
     url(r'^home/', include('home.urls')),
     url(r'^admin/', admin.site.urls),
