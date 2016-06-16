42a462af1f78983af30d86fa30ebc4f3ae9a38b9
Carlos Gonzalez <carlos.dgonzalezc46@gmail.com>
2016-06-16 16:55:06 -0430

    Recargar saldo en billetera cliente y autenticacion de tarjeta de credito

--- AutomatizacionRestaurante/cliente/forms.py
+++ AutomatizacionRestaurante/cliente/forms.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from django.forms import EmailField, ModelForm, CharField, Form, FloatField
+from django.forms import EmailField, ModelForm, CharField, Form, FloatField, \
+RegexField
 from django.contrib.auth.models import User
 from django.forms.widgets import PasswordInput, NumberInput
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Field
+
 
 from .models import Billetera
 
@@ -57,11 +62,42 @@ class ClaveBilleteraForm(Form):
 
 
 class RecargaBilleteraForm(Form):
-    #class Meta:
-        #model = Billetera 
-        #fields = ['creditos']
-
+    def __init__(self, *args, **kwargs):
+        super(RecargaBilleteraForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_id = 'recargar_biletera_form'
+        self.helper.form_class = 'forms col-md-8'
+        self.helper.form_method = 'POST'
+        self.helper.add_input(Submit('submit', 'Recargar', css_class='btn-success'))
+        self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn btn-default'))
+        self.helper.layout = Layout(
+            Field('numero', placeholder='e.g. 0000-0000-0000-0000'),
+            Field('nombre', placeholder='Nombre'),
+            Field('codigo', placeholder='Codigo'),
+            Field('monto', placeholder='Monto'))
+        
     monto = FloatField(
         label='monto',
-        widget=NumberInput(),
+        widget=NumberInput(attrs={ 'min': '0'}),
         error_messages={'required':'Este campo es requerido.'})
+
+    nombre = RegexField(
+        label='Nombre en Tarjeta',
+        regex=r'^([a-zA-Z]+|[a-zA-Z]+\s[a-zA-Z]+)$',
+        error_messages={
+            'invalid': 'El nombre de usuario sólo puede tener letras',
+            'required': 'Este campo es requerido.'})
+
+    numero = RegexField(
+        label='Numero Tarjeta',
+        regex=r'^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$',
+        error_messages={
+            'invalid': 'El número de la tarjeta debe tener este formato: 0000-0000-0000-0000',
+            'required': 'Este campo es requerido.'})
+
+    codigo = RegexField(
+        label='Codigo de Seguridad',
+        regex=r'^[0-9]{3}$',
+        error_messages={
+            'invalid': 'El código de seguridad tiene que ser de 3 dígitos',
+            'required': 'Este campo es requerido.'})
